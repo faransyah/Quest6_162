0a8666539c44b012c1d41e7c1a1680c9bfb6fca4
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-22 00:23:00 +0700

    membuat text Pilih Kelas Belajar

--- app/src/main/java/com/example/navigationcompose/ui/View/Screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/View/Screen/RencanaStudyView.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -121,7 +122,11 @@ fun RencanaStudyView(
                         chosenDropdown = it
                     }
                 )
-                Spacer()
+                Spacer(modifier = Modifier.padding(8.dp))
+                HorizontalDivider()
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(text = "Pilih Kelas Belajar", fontWeight = FontWeight.Bold)
+                Text()
 
             }
         }
