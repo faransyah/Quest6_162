9f3777f2f421dc3c3a855e9a7465c1cb829f30d6
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-21 21:54:09 +0700

    membuat fungsi fungsi savedatakrs dengan mutable list untuk menyimpan nama matakuliah serta kelas

--- app/src/main/java/com/example/navigationcompose/ui/View/ViewModel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigationcompose/ui/View/ViewModel/RencanaStudyViewModel.kt
@@ -7,17 +7,24 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class RencanaStudyViewModel : ViewModel(){
+class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 
-    fun setMataKuliah(mkPilihan: String){
+    fun setMataKuliah(mkPilihan: String) {
         _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
     }
 
-    fun setKelas(kelasPilihan: String){
+    fun setKelas(kelasPilihan: String) {
         _krsState.update { stateKelas -> stateKelas.copy(kelas = kelasPilihan) }
     }
 
-
+    fun saveDataKRS(ls: MutableList<String>) {
+        _krsState.update { status ->
+            status.copy(
+                namaMK = ls[0],
+                kelas = ls[1]
+            )
+        }
+    }
 }
