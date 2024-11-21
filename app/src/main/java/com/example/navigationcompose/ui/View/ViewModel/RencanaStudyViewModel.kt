07dd3aae40beb4ff365e6d1902360332ce0459b8
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-21 21:49:32 +0700

    membuat fungsi  setmatakuliah

--- app/src/main/java/com/example/navigationcompose/ui/View/ViewModel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/navigationcompose/ui/View/ViewModel/RencanaStudyViewModel.kt
@@ -5,11 +5,15 @@ import com.example.navigationcompose.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel(){
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
 
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
+    }
 
 
 }
