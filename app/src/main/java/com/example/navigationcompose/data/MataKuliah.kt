d9be2daee5cd11526cff84284bdd9fe1675ac9bc
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-21 20:49:57 +0700

    Mengisi object matakuliah menggunakan variable Listof

--- app/src/main/java/com/example/navigationcompose/data/MataKuliah.kt
+++ app/src/main/java/com/example/navigationcompose/data/MataKuliah.kt
@@ -1,4 +1,12 @@
 package com.example.navigationcompose.data
 
 object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdesan Buatan",
+        "Computer Vission"
+    )
 }
