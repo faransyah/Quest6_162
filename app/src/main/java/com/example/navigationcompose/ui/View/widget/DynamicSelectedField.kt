f93dff8d7b5435bb84197353b602f41bb6799f19
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-21 21:01:35 +0700

    mengisi fungsi tersebut

--- app/src/main/java/com/example/navigationcompose/ui/View/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationcompose/ui/View/widget/DynamicSelectedField.kt
@@ -1,10 +1,17 @@
 package com.example.navigationcompose.ui.View.widget
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import java.util.prefs.NodeChangeEvent
 
 @Composable
 fun DynamicSelectedTextField(
     selectedValue: String,
     options: List<String>,
+    label: String,
+    onValueChangeEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
 
-)
+){
+
+}
