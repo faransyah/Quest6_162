62b9cd3294d29ab5abb29a1d65c2a59a9b408359
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-22 00:18:04 +0700

    Membuat Column baru

--- app/src/main/java/com/example/navigationcompose/ui/View/Screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/View/Screen/RencanaStudyView.kt
@@ -98,6 +98,14 @@ fun RencanaStudyView(
                     )
                 )
                 .fillMaxSize(),
-        )
+        ){
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Text()
+            }
+        }
     }
 }
