0773dc26022de8757181cb9bf16214fb116aa57d
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-21 23:53:53 +0700

    membuat variabel chosendropdown

--- app/src/main/java/com/example/navigationcompose/ui/View/Screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/View/Screen/RencanaStudyView.kt
@@ -1,2 +1,17 @@
 package com.example.navigationcompose.ui.View.Screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onBackButtonClicked: () -> Unit
+
+){
+    var chosenDropdown by rember {
+        mutableStateOf(
+
+        )
+    }
+}
