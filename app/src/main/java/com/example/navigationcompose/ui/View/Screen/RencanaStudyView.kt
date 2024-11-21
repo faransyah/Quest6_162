25d2628faa6ab853a7c039492a130eefedb4d5e5
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-22 00:09:12 +0700

    menambahkan spacer

--- app/src/main/java/com/example/navigationcompose/ui/View/Screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/View/Screen/RencanaStudyView.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
@@ -52,6 +54,7 @@ fun RencanaStudyView(
                     .clip(shape = CircleShape)
                     .size(50.dp)
             )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
 
 
         }
