5ae47fa42562ee94ca93b0f12f9c3c138aa0fe59
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-22 15:07:53 +0700

    Membuat Column untuk di isi dengan text

--- app/src/main/java/com/example/navigationcompose/ui/View/Screen/TampilanView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/View/Screen/TampilanView.kt
@@ -1,19 +1,30 @@
 package com.example.navigationcompose.ui.View.Screen
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.navigationcompose.R
 import com.example.navigationcompose.model.Mahasiswa
 import com.example.navigationcompose.model.RencanaStudi
@@ -43,7 +54,45 @@ fun TampilScreen(
                     .clip(shape = CircleShape)
                     .size(50.dp)
             )
+            Spacer(modifier = Modifier .padding(start = 16.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+                Box {
+                    Icon(
+                        imageVector = Icons.Filled.Notifications,
+                        contentDescription = "",
+                        tint = Color.White
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .background( color = Color.White,
+                            shape = RoundedCornerShape(
+                                topEnd = 15.dp,
+                                topStart = 15.dp
+                            )
+                        )
+                        .fillMaxSize()
+                ){
+                    Column(modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally) {
 
+                    }
+                }
+            }
         }
     }
 }
