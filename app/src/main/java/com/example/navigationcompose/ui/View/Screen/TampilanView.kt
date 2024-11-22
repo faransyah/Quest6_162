25b453e7bc49f3566e6c4ebed25624e1ad246dde
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-22 14:09:13 +0700

    membuat Tampilan screen dan parameter

---
+++ app/src/main/java/com/example/navigationcompose/ui/View/Screen/TampilanView.kt
@@ -0,0 +1,15 @@
+package com.example.navigationcompose.ui.View.Screen
+
+import androidx.compose.runtime.Composable
+import com.example.navigationcompose.model.Mahasiswa
+import com.example.navigationcompose.model.RencanaStudi
+
+@Composable
+fun TampilScreen(
+    mahasiswa: Mahasiswa,
+    rencanaStudi: RencanaStudi,
+    onBackButton: () -> Unit
+){
+
+
+}
