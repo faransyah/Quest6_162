7025bba76fefdffb42712d8a3590ac13210064b9
faransyah <m.faransyah.ft22@mail.umy.ac.id>
2024-11-22 14:52:10 +0700

    membuat Row serta membuat image untuk mengisi logi

--- app/src/main/java/com/example/navigationcompose/ui/View/Screen/TampilanView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/View/Screen/TampilanView.kt
@@ -1,6 +1,20 @@
 package com.example.navigationcompose.ui.View.Screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.navigationcompose.R
 import com.example.navigationcompose.model.Mahasiswa
 import com.example.navigationcompose.model.RencanaStudi
 
@@ -10,6 +24,26 @@ fun TampilScreen(
     rencanaStudi: RencanaStudi,
     onBackButton: () -> Unit
 ){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Alignment.CenterVertically
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
 
-
+        }
+    }
 }
